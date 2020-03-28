f9acdeffbdc193ed6dd89def837f9fd4975513ed
akshaybabbar12@gmail.com <akshaybabbar12@gmail.com>
2020-03-28 19:30:53 +0530

    ClientExecutorParserImpl ClientExecutorParser  Services impl

---
+++ src/main/java/com/zomato/clone/omservice/ClientExecutorParser.java
@@ -0,0 +1,13 @@
+package com.zomato.clone.omservice;
+
+import com.zomato.clone.model.PaymentEntity;
+
+public interface ClientExecutorParser {
+
+    public PaymentEntity paymentExcuterParser();
+
+    public void realTimeTrackingExecutorParser();
+
+    public void ratingReviewExecutorParser();
+
+}
---
+++ src/main/java/com/zomato/clone/omservice/serviceimpl/ClientExecutorParserImpl.java
@@ -0,0 +1,23 @@
+package com.zomato.clone.omservice.serviceimpl;
+
+import com.zomato.clone.model.PaymentEntity;
+import com.zomato.clone.omservice.ClientExecutorParser;
+
+public class ClientExecutorParserImpl implements ClientExecutorParser {
+
+
+    @Override
+    public PaymentEntity paymentExcuterParser() {
+        return null;
+    }
+
+    @Override
+    public void realTimeTrackingExecutorParser() {
+
+    }
+
+    @Override
+    public void ratingReviewExecutorParser() {
+
+    }
+}
