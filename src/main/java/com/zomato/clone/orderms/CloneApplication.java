d7aa58d890f351f736d0250d0aaf4b236faee04f
akshayakaushik <kaushikakshaya03@gmail.com>
2020-03-28 09:18:06 +0530

    Correcting the project Structure and main class

--- src/main/java/com/zomato/clone/orderms/CloneApplication.java
+++ src/main/java/com/zomato/clone/orderms/CloneApplication.java
@@ -2,8 +2,10 @@ package com.zomato.clone.orderms;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = "com.zomato.clone")
 public class CloneApplication {
 
 	public static void main(String[] args) {
